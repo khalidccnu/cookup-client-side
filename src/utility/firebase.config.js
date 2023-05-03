25b760305c00702982f4ff6c635e4c3ba336e2b9
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-03 15:19:17 +0600

    firebase added

---
+++ src/utility/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const config = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+};
+
+const app = initializeApp(config);
+export const auth = getAuth(app);
